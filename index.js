8ba774d7a88b203365cc0fe0bc1d12a2b7a9bbbb
Igor Risteski <pirej30@yahoo.com>
2020-05-13 03:35:49 +0200

    Express, basic setup listen to port 3000 http://localhost:3000/

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hi there");
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hi there, how are you?');
+});
+
+app.listen(3000, () => {
+    console.log('Listening!!');
+});
+
+
+
+
+
+
