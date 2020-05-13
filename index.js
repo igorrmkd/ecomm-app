e69c8571cc90345646e83195d2407f80828bd5a5
Igor Risteski <pirej30@yahoo.com>
2020-05-13 23:08:53 +0200

    get access to the form data submitted,

--- index.js
+++ index.js
@@ -16,6 +16,29 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
+    // get access to the form data submitted
+    req.on('data', data => {
+        // the data we get needs converting to strings,
+        // and split by the & symbol
+        const parsed = data.toString('utf8').split('&');
+
+        const formData = {};
+        // loop through parsed data, and iuse the values 
+        // from both sides of = as key and value
+        // email=pero%40mail.comd&password=dddddddddd&passwordConfirmation=kkkkkkkkkk
+        for (let pair of parsed) {
+            const [key, value] = pair.split('=');
+            formData[key] = value;
+        }
+        //we get this as formData:
+        // {
+        //     email: 'pero%40mail.comd',
+        //     password: 'dddddddddd',
+        //     passwordConfirmation: 'kkkkkkkkkk'
+        // }
+        console.log(formData);
+
+    });
     res.send('Account created!!!');
 });
 
