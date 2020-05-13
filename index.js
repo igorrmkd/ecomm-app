f73ae6e3f51ad7d959dc140a61d2fc8854b404c2
Igor Risteski <pirej30@yahoo.com>
2020-05-13 23:30:38 +0200

    Middleware, create it manually

--- index.js
+++ index.js
@@ -15,30 +15,41 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.post('/', (req, res) => {
-    // get access to the form data submitted
-    req.on('data', data => {
-        // the data we get needs converting to strings,
-        // and split by the & symbol
-        const parsed = data.toString('utf8').split('&');
+// this is a middleware, reusable "get the data" function..
+const bodyParser = (req, res, next) => {
+    if (req.method === 'POST') {
+        // get access to the form data submitted
+        req.on('data', data => {
+            // the data we get needs converting to strings,
+            // and split by the & symbol
+            const parsed = data.toString('utf8').split('&');
 
-        const formData = {};
-        // loop through parsed data, and iuse the values 
-        // from both sides of = as key and value
-        // email=pero%40mail.comd&password=dddddddddd&passwordConfirmation=kkkkkkkkkk
-        for (let pair of parsed) {
-            const [key, value] = pair.split('=');
-            formData[key] = value;
-        }
-        //we get this as formData:
-        // {
-        //     email: 'pero%40mail.comd',
-        //     password: 'dddddddddd',
-        //     passwordConfirmation: 'kkkkkkkkkk'
-        // }
-        console.log(formData);
+            const formData = {};
+            // loop through parsed data, and iuse the values 
+            // from both sides of = as key and value
+            // email=pero%40mail.comd&password=dddddddddd&passwordConfirmation=kkkkkkkkkk
+            for (let pair of parsed) {
+                const [key, value] = pair.split('=');
+                formData[key] = value;
+            }
+            //we get this as formData:
+            // {
+            //     email: 'pero%40mail.comd',
+            //     password: 'dddddddddd',
+            //     passwordConfirmation: 'kkkkkkkkkk'
+            // }
+            req.body = formData;
+            next(); // to tell Express taht we are done here, move on..
 
-    });
+        });
+    } else {
+        next();
+    }
+};
+
+// first run the bodyParser(middleware) - function, and then the callback
+app.post('/', bodyParser, (req, res) => {
+    console.log(req.body);
     res.send('Account created!!!');
 });
 
