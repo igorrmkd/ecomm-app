878b80fc6708f9918f86f82486eceda08bdda883
Igor Risteski <pirej30@yahoo.com>
2020-05-13 22:43:49 +0200

    Add a simple form to the page, change the default GET method in to POST, show a response message to the submitted(posted) form data -> "Account created!"

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hi there, how are you?');
+    res.send(`
+        <div>
+        <form method="Post">
+                <input name="email" placeholder="email" /><br>
+                <input name="password" placeholder="password" /><br>
+                <input name="passwordConfirmation" placeholder="password confirmation" /><br>
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Account created!!!');
 });
 
 app.listen(3000, () => {
