40e2193c12eea0ca3bd74486a7da544618fe557d
Igor Risteski <pirej30@yahoo.com>
2020-05-23 02:44:20 +0200

    Products form Display initial (new)Products form.

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const productsRepo = require('../../repositories/products');
+const productsNewTemplate = require('../../views/admin/products/new');
 
 const router = express.Router();
 
 /// list the products to the administrator
 router.get('/admin/products', (req, res) => {
-    res.send('alooo products');
+    res.send(productsNewTemplate({}));
 });
 
 /// create new product
 router.get('/admin/products/new', (req, res) => {
-    res.send('alooo new products');
+    res.send(productsNewTemplate({}));
 });
 
 module.exports = router;
