082876708888ca53cf83b8eaaa4b1ae9020b5b21
Igor Risteski <pirej30@yahoo.com>
2020-05-23 02:20:02 +0200

    Products creation, initial implementation

--- repositories/users.js
+++ repositories/users.js
@@ -1,25 +1,25 @@
 const fs = require('fs');
 const crypto = require('crypto');
 const util = require('util');
+const Repository = require('./repository');
 
 // promisify - return a version of the function that returns a prommise
 const scrypt = util.promisify(crypto.scrypt);
 
-class UsersRepository {
-    constructor(filename) {
-        if (!filename) {
-            throw new Error('Creating a repository requieres a filename');
-        }
+// add "extends Repository" to get access to all of the Repository methods
+class UsersRepository extends Repository {
+    async comparePasswords(saved, supplied) {
+        /// saved -> password saved in our database, "hashed.salt"
+        /// supplied -> password given to us by the user trying to sign in
+        // const result = saved.split('.');
+        // const hashed = result[0];
+        // const salt = result[1];
 
-        this.filename = filename;
-        try {
-            fs.accessSync(this.filename);
-        } catch (err) {
-            fs.writeFileSync(this.filename, '[]');
-        }
-    }
-    async getAll() {
-        return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
+        const [hashed, salt] = saved.split('.'); // destructure..
+        // combine supplied and salt, and hash them
+        const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+        // compare and return true or false
+        return hashed === hashedSuppliedBuf.toString('hex');
     }
 
     async create(attrs) {
@@ -44,74 +44,12 @@ class UsersRepository {
 
         return record; // get an object including the id for the user we made
     }
-
-    async comparePasswords(saved, supplied) {
-        /// saved -> password saved in our database, "hashed.salt"
-        /// supplied -> password given to us by the user trying to sign in
-        // const result = saved.split('.');
-        // const hashed = result[0];
-        // const salt = result[1];
-
-        const [hashed, salt] = saved.split('.'); // destructure..
-        // combine supplied and salt, and hash them
-        const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
-        // compare and return true or false
-        return hashed === hashedSuppliedBuf.toString('hex');
-    }
-
-    async writeAll(records) {
-        // write the updated 'records' array, back to this.filename (users.json)
-        await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
-    }
-
-    randomId() {
-        return crypto.randomBytes(4).toString('hex');
-    }
-
-    async getOne(id) {
-        const records = await this.getAll();
-        return records.find(record => record.id === id);
-    }
-
-    async delete(id) {
-        const records = await this.getAll();
-        const filteredRecords = records.filter(record => record.id !== id);
-        await this.writeAll(filteredRecords);
-    }
-
-    async update(id, attrs) {
-        const records = await this.getAll();
-        const record = records.find(record => record.id === id);
-
-        if (!record) {
-            throw new Error(`Record with id ${id}, not found`)
-        }
-        // add the new attrs(new mail, pass, or..) to the existing record we found by id
-        Object.assign(record, attrs);
-        await this.writeAll(records);
-    }
-
-    async getOneBy(filters) {
-        const records = await this.getAll();
-
-        for (let record of records) {
-            let found = true;
-
-            for (let key in filters) {
-                if (record[key] !== filters[key]) {
-                    found = false;
-                }
-            }
-
-            if (found) {
-                return record;
-            }
-        }
-    }
-
 }
 
 module.exports = new UsersRepository('users.json');
+
+
+
 
 // const test = async () => {
 //     const repo = new UsersRepository('users.json');
@@ -137,6 +75,4 @@ module.exports = new UsersRepository('users.json');
 // };
 
 // test();
-
-
 
