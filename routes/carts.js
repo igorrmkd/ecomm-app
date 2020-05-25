30b904cf7e6e582594670ce7c45a7ad8dc6dde7e
Igor Risteski <pirej30@yahoo.com>
2020-05-25 16:44:10 +0200

    Shopping cart preparation

---
+++ routes/carts.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Receive a post request to add an item to cart
+
+
+// Receive a get request to show all items in cart
+
+
+// Receive a post request to delete an item from cart
+
+
+
+module.exports = router;
+
